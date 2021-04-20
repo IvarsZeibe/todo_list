f13b8e7b97babbd323cf3da7e15cb2153c6a1bc0
Ivars Žeibe <Ivars_Zeibe@hotmail.com>
2021-04-20 15:18:05 +0300

    Uzdevumu pievienošana

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ function remove(button) {
 function edit(button) {
 
 }
+function add() {
+    form = document.getElementById(newTodoForm);
+    todo = document.createElement("div");
+    todo.innerHTML = `
+        <div class="taskBox">
+            <span class="task">${document.getElementById("newTask").value}</span>
+            <span class="dueDate">${document.getElementById("newDueDate").value}</span>
+            <button id="RemoveButton" onclick="remove(this)">Dzēst</button>
+            <button id="EditButton" onclick="edit(this)">Rediģēt</button>
+        </div>`;
+    document.getElementById("taskCanvas").insertBefore(todo, document.getElementById("openForm"));
+    document.getElementById("overlay").style.display = "none";
+}
+function openForm(){
+    document.getElementById("overlay").style.display = "flex";
+}
